c5c846db38362f1a21b3bd15aadfbc54ef2ade8f
Kratika Gupta <201377216+kritzi13@users.noreply.github.com>
2025-07-06 16:12:56 +0530

    Create CountingDigits.java

---
+++ CountingDigits.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        int count = 0;
+
+        if (num == 0) {
+            count = 1;
+        } else {
+            while (num != 0) {
+                num = num / 10;
+                count++;
+            }
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
