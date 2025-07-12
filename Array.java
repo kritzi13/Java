f8157536c6781418bffe5a9fa95326a4a9558ca3
Kratika Gupta <201377216+kritzi13@users.noreply.github.com>
2025-07-12 21:38:26 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,1 +1,20 @@
+//Easy example to understand Arrays
+import java.util.Scanner;
+import java.util.Arrays;
+public class ques1 {
+    public static void main(String[] arg){
+        Scanner in = new Scanner(System.in);
 
+        System.out.print("Enter number of students: ");
+        int size = in.nextInt();
+        String[] name = new String[size];
+
+        for (int i= 0; i <size; i++){
+            System.out.print("Student name: ");
+            name[i] = in.next();
+        }
+
+        System.out.println(Arrays.toString(name));
+    }
+    
+}
